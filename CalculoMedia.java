40fb3133e619f6b34a95d677496415e871e5d01c
Rafael Soldatelli da Rosa <105215757+RafaSoldatelli@users.noreply.github.com>
2024-09-14 17:48:39 -0300

    Add files via upload

---
+++ CalculoMedia.java
@@ -0,0 +1,18 @@
+package TarefaCalculoMedia;
+
+public class CalculoMedia {
+
+    public static void main(String[] args) {
+
+        int nota1 = 2;
+        int nota2 = 3;
+        int nota3 = 6;
+        int nota4 = 8;
+
+        int soma = nota1 + nota2 + nota3 + nota4;
+
+        double media = soma / 4.0;
+
+        System.out.println("A média das notas é: " + media);
+    }
+}
