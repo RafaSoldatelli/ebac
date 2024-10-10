b8349c26a0fa9dda4609cf799ce4423c8824e71c
Rafael Soldatelli  da Rosa <you@example.com>
2024-10-10 11:05:48 -0300

    Adding wrapperTask

---
+++ tarefa_wrapper.java
@@ -0,0 +1,12 @@
+package TarefaCalculoMedia;
+
+public class tarefa_wrapper {
+    public static void main(String[] args) {
+
+        int num1 = 1000000;
+        System.out.println(num1);
+
+        int num2 = (short) num1;
+        System.out.println(num2);
+    }
+}
