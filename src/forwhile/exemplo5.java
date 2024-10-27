ecb1b2fc50abc5622113077c69c491037d25f4c3
Rafael Soldatelli  da Rosa <you@example.com>
2024-10-27 13:08:37 -0300

    Aula ebac

--- src/forwhile/exemplo5.java
+++ src/forwhile/exemplo5.java
@@ -2,22 +2,21 @@ package forwhile;
 
 import java.util.Scanner;
 
-public class exemplo4 {
+public class exemplo5 {
     public static void main(String args[]) {
         Scanner s = new Scanner(System.in);
 
-        System.out.println("Deseja gerar a tabuada de algum número? ");
-        String resposta = s.next();
-
-        while(resposta.equalsIgnoreCase("Sim")) {
-            System.out.println("Qual é o número da tabuada?");
+        String resposta = "";
+        do {
+            System.out.println("Digite um número para gerar a tabuada: ");
             int num = s.nextInt();
             for (int i = 0; i <= 10; i++) {
                 System.out.println(num + " x " + i + " = " + num * i);
             }
             System.out.println("Deseja gerar novamente a tabuada de algum número? ");
             resposta = s.next();
-        }
-        System.out.println("Obrigado. ");
+        } while (resposta.equalsIgnoreCase("Sim"));
+
+        System.out.println("Obrigado!");
     }
 }
