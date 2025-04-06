49aaa5d9717507fd18c182634be11c88e9725538
Rafael Soldatelli  da Rosa <you@example.com>
2025-04-06 16:10:55 -0300

    Aula 13

--- ebac-master/ebac-master/src/atividades/modulo13/Pessoa.java
+++ ebac-master/ebac-master/src/atividades/modulo13/Pessoa.java
@@ -1,4 +1,17 @@
 package atividades.modulo13;
 
 public class Pessoa {
+    private String nome;
+
+    public Pessoa(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String SetNome(String nome) {
+        return this.nome = nome;
+    }
 }
--- ebac-master/ebac-master/src/atividades/modulo13/PessoaJuridica.java
+++ ebac-master/ebac-master/src/atividades/modulo13/PessoaJuridica.java
@@ -1,4 +1,26 @@
 package atividades.modulo13;
 
-public class PJ {
+public class PessoaJuridica extends Pessoa{
+    private String cnpj;
+
+    public PessoaJuridica(String nome, String cnpj) {
+        super(nome);
+        this.cnpj = cnpj;
+    }
+
+    public String GetCnpj() {
+        return cnpj;
+    }
+
+    public void SetCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    @Override
+    public String toString() {
+        return "PessoaJuridica{" +
+                "cnpj='" + cnpj + '\'' +
+                '}';
+    }
+
 }
